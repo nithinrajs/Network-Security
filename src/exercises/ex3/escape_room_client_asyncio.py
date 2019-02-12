fab0ffeba3d317ef7e83698d60f8304b94ad0b99
Nithin Sade <sadenithin@gmail.com>
2019-02-11 21:07:11 -0500

    Create escape_room_client_asyncio.py
    ASYNCIO CLIENT

---
+++ src/exercises/ex3/escape_room_client_asyncio.py
@@ -0,0 +1,28 @@
+import asyncio
+
+class ClientProtocol(asyncio.Protocol):
+    def __init__(self, loop):
+        self.loop = loop
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        print(data.decode())
+        self.message = input(">> ")
+        self.transport.write(self.message.encode())
+
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        print('GAME OVER!')
+        self.loop.stop()
+
+loop = asyncio.get_event_loop()
+coro = loop.create_connection(lambda: ClientProtocol(loop),'', 9999)
+loop.run_until_complete(coro)
+try:
+    loop.run_forever()
+except:
+    pass
+
+loop.close()
