d3b8c40daa27b651f94e23b7dc518a37304826b0
Nithin Sade <nsade1@jhu.edu>
2019-03-04 17:47:12 -0500

    Create escape_room_client_playground.py

---
+++ src/exercises/ex6/escape_room_client_playground.py
@@ -0,0 +1,52 @@
+import asyncio
+import sys
+import playground
+
+stdin_queue = []
+
+
+def get_user_data():
+    line_in = sys.stdin.readline()
+    line_in = line_in[:-1]
+    stdin_queue.append(line_in)
+
+
+async def async_input(prompt):
+ print(prompt, end="")
+ sys.stdout.flush()
+ while len(stdin_queue) == 0:
+    await asyncio.sleep(.1)
+ return stdin_queue.pop(0)
+
+async def game_runner(protocol):
+        while True:
+            command = await async_input(">> ")
+            protocol.transport.write(command.encode())
+            await asyncio.sleep(.1)  #Temporary
+            
+
+class ClientProtocol(asyncio.Protocol):
+    def __init__(self, loop):
+        self.loop = loop
+    
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def data_received(self, data):
+        print(data.decode())
+        
+        #self.message = get_user_data()
+
+
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        print('GAME OVER!')
+        self.loop.stop()
+
+loop = asyncio.get_event_loop()
+coro = playground.create_connection(lambda: ClientProtocol(loop),'20191.100.200.300', 9999) #Address of my VNIC
+transport, protocol = loop.run_until_complete(coro)
+loop.add_reader(sys.stdin, get_user_data)
+asyncio.ensure_future(game_runner(protocol))
+loop.run_forever()
+loop.close()
